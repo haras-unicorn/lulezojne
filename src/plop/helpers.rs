a94689678a15ea4f2e858cf0ec5392b1931db217
haras <social@hrvojej.anonaddy.me>
2023-10-15 14:25:16 +0200

    add saturation and lightness helpers

--- src/plop/helpers.rs
+++ src/plop/helpers.rs
@@ -53,10 +53,72 @@ handlebars_helper!(SetAlphaHelper: |color: Rgba, alpha: f32| {
   })
 });
 
+handlebars_helper!(SetLightnessHelper: |color: Rgba, lightness: f32| {
+  let Rgba {
+    red,
+    green,
+    blue,
+    alpha
+  } = color;
+  let mut hsla = palette::IntoColor::<palette::Okhsla>::into_color(
+    palette::Alpha::<palette::LinSrgb<u8>, f32>::new(red, green, blue, alpha)
+      .into_format::<f32, f32>(),
+  );
+  hsla.lightness = lightness;
+  let processed = palette::IntoColor::<palette::LinSrgba>::into_color(hsla).into_format::<u8, f32>();
+  let palette::Alpha::<palette::LinSrgb::<u8>, f32> {
+    color: palette::LinSrgb::<u8> {
+      red,
+      green,
+      blue,
+      ..
+    },
+    alpha
+  } = processed;
+  json!(Rgba {
+    red,
+    green,
+    blue,
+    alpha
+  })
+});
+
+handlebars_helper!(SetSaturationHelper: |color: Rgba, saturation: f32| {
+  let Rgba {
+    red,
+    green,
+    blue,
+    alpha
+  } = color;
+  let mut hsla = palette::IntoColor::<palette::Okhsla>::into_color(
+    palette::Alpha::<palette::LinSrgb<u8>, f32>::new(red, green, blue, alpha)
+      .into_format::<f32, f32>(),
+  );
+  hsla.saturation = saturation;
+  let processed = palette::IntoColor::<palette::LinSrgba>::into_color(hsla).into_format::<u8, f32>();
+  let palette::Alpha::<palette::LinSrgb::<u8>, f32> {
+    color: palette::LinSrgb::<u8> {
+      red,
+      green,
+      blue,
+      ..
+    },
+    alpha
+  } = processed;
+  json!(Rgba {
+    red,
+    green,
+    blue,
+    alpha
+  })
+});
+
 pub fn register(handlebars: &mut handlebars::Handlebars) {
   handlebars.register_helper("hex", Box::new(HexHelper));
   handlebars.register_helper("hexa", Box::new(HexaHelper));
   handlebars.register_helper("rgb", Box::new(RgbHelper));
   handlebars.register_helper("rgba", Box::new(RgbaHelper));
   handlebars.register_helper("set-alpha", Box::new(SetAlphaHelper));
+  handlebars.register_helper("set-lightness", Box::new(SetLightnessHelper));
+  handlebars.register_helper("set-saturation", Box::new(SetSaturationHelper));
 }
